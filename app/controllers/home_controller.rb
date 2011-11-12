5c8a1565b90e4ae81bdd8c9d7aaf2ca2c7c8bec6
Oitenta <pedro.faria.80@gmail.com>
2011-11-12 00:18:58 +0000

    New Charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,18 @@ class HomeController < ApplicationController
 	@searches = Search.all
   end
   
+  def graphicspie
+    @users = User.all
+    @user = current_user
+	@searches = Search.all
+  end
+
+  def graphicscolumns
+    @users = User.all
+    @user = current_user
+	@searches = Search.all
+  end
+
   def graficos
     @users = User.all
     @user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Delusion::Application.routes.draw do
   	devise_for :users
 
     match '/graficos', :to => 'home#graficos'
-
-  	root :to => "home#index"
+	match '/graphicspie', :to => 'home#graphicspie'
+	match '/graphicscolumns', :to => 'home#graphicscolumns'
+  	
+	root :to => "home#index"
 end
