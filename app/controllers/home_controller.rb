ed4e183b9b3b82699762321212a1ddc277a79f86
Oitenta <pedro.faria.80@gmail.com>
2011-11-14 16:19:20 +0000

    up searches! area added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,18 +8,38 @@ class HomeController < ApplicationController
   def graphicspie
     @users = User.all
     @user = current_user
+	save_search("Pie Chart")
 	@searches = Search.all
   end
 
   def graphicscolumns
     @users = User.all
     @user = current_user
+	save_search("Column Chart")
+	@searches = Search.all
+  end
+
+  def graphicsarea
+    @users = User.all
+    @user = current_user
+	save_search("Area Chart")
 	@searches = Search.all
   end
 
   def graficos
     @users = User.all
     @user = current_user
+	save_search("Linear Chart")
 	@searches = Search.all
   end
+
+  private
+    def save_search (tipo)
+	  provse = Search.new
+	  provse.tipo = tipo
+	  provse.date = Time.now.to_datetime
+	  provse.user_id = current_user.id
+	  provse.atributos = "none"
+	  provse.save
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Delusion::Application.routes.draw do
     match '/graficos', :to => 'home#graficos'
 	match '/graphicspie', :to => 'home#graphicspie'
 	match '/graphicscolumns', :to => 'home#graphicscolumns'
+	match '/graphicsarea', :to => 'home#graphicsarea'
   	
 	root :to => "home#index"
 end
